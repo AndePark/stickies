2a762db08bc546a3271ad022a81253c035fd6b64
AndePark <andrewpark2396@gmail.com>
2025-09-21 12:29:56 -0700

    initial commit

---
+++ Stickies/src/main/java/com/example/Stickies/StickiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Stickies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickiesApplication.class, args);
+	}
+
+}
